3c07ffdc0ff7771c449225e2fe94e4aa6bd52d91
dpon1996 <dpon1996@gmail.com>
2022-04-27 10:50:32 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:interview/screens/splashScreen/splashScreen.dart';
+
+void main() {
+  runApp(
+    const MyApp(),
+  );
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        scaffoldBackgroundColor: Colors.white,
+      ),
+      home: const SplashScreen(),
+    );
+  }
+}
---
+++ lib/res/colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+const primaryColor = Color(0xff6D9470);
+const appGreen = Color(0xff20BE79);
+const cartGreen = Color(0xffDEF9EC);
+const appBlue = Color(0xff3C94FC);
+const appRed = Color(0xffFE5050);
+const appYellow = Color(0xffFFC949);
+const appOrange = Color(0xffFFC949);
+const appGrey = Color(0xffEFEFEF);
